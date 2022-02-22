0307b349ef72d118e95f1ca2cee1b2db7f34d9e4
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-02-22 10:27:32 -0800

    handling file uploads

--- app/main.py
+++ app/main.py
@@ -1,26 +1,26 @@
 import pathlib
 import os
 import io
-
+import uuid
 from functools import lru_cache
-
 from fastapi import(
     FastAPI,
-    # Header,
-    # HTTPException,
+    Header,
+    HTTPException,
     Depends,
     Request,
-    # File,
-    # UploadFile
+    File,
+    UploadFile
     )
-from fastapi.responses import HTMLResponse
+import pytesseract
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
-
+from PIL import Image
 class Settings(BaseSettings):
     # app_auth_token: str
     debug: bool = False
-    # echo_active: bool = False
+    echo_active: bool = False
     # app_auth_token_prod: str = None
     # skip_auth: bool = False
 
@@ -35,7 +35,7 @@ settings = get_settings()
 DEBUG=settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
-# UPLOAD_DIR = BASE_DIR / "uploads"
+UPLOAD_DIR = BASE_DIR / "uploads"
 
 
 app = FastAPI()
@@ -44,3 +44,26 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
+
+@app.post("/") # http POST
+def home_detail_view():
+    return {"hello": "world"}
+
+@app.post("/img-echo/", response_class=FileResponse) # http POST
+async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="Invalid endpoint", status_code=400)
+    UPLOAD_DIR.mkdir(exist_ok=True) # create directory if needed
+    bytes_str = io.BytesIO(await file.read()) # read image data
+    # try:
+    #     img = Image.open(bytes_str)
+    # except:
+    #     raise HTTPException(detail="Invalid image", status_code=400)
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix # .jpg, .txt
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}" # use string substitution
+
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    # img.save(dest)
+    return dest
