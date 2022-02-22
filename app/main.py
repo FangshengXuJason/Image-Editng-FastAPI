2b6607347db0107fbd63ce689b2ad947aa7c0a93
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-02-22 09:48:10 -0800

    just a commit

--- app/main.py
+++ app/main.py
@@ -7,6 +7,18 @@ from fastapi import(
     )
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    # app_auth_token: str
+    debug: bool = False
+    # echo_active: bool = False
+    # app_auth_token_prod: str = None
+    # skip_auth: bool = False
+
+    class Config:
+        env_file = ".env"
+
 
 BASE_DIR = pathlib.Path(__file__).parent
 # UPLOAD_DIR = BASE_DIR / "uploads"
